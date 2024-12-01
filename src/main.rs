6ed5fd2ccf45eb089554107d6f13f34adf0ea3ce
Titouan Teyssier <titouan.teyssier@gmail.com>
2024-12-01 12:21:24 +0100

    change repo name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate aoc_rust;
+extern crate aoc_rust_2024;
 extern crate aoc_runner_derive;
 extern crate aoc_runner;
 
 use aoc_runner_derive::aoc_main;
 
-aoc_main! { lib = aoc_rust }
+aoc_main! { lib = aoc_rust_2024 }
