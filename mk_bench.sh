37f176045186e5252ff3d746abe9b2c0fa788f06
Titouan Teyssier <titouan.teyssier@gmail.com>
2024-12-03 21:29:40 +0100

    auto hide

--- mk_bench.sh
+++ mk_bench.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-for day in $(seq 1 $1)
+YEAR=2024
+# maybe query that from Cargo.toml
+REPO=advent_of_code_rust_2024
+need_hiding=0
+for dayi in $(seq 1 $1)
 do
-    day="day${day}"
-    sed "s:__DAY__:${day}:g ; s:__YEAR__:2024:g ; s:__REPO__:advent_of_code_rust_2024:g" templates/day.rs > "benches/${day}.rs"
+    day="day${dayi}"
+    sed "s:__DAY__:${day}:g ; s:__YEAR__:${YEAR}:g ; s:__REPO__:${REPO}:g" templates/day.rs > "benches/${day}.rs"
     grep -q "${day}" Cargo.toml || sed "s:__DAY__:${day}:g" templates/cargo_bench.toml >> Cargo.toml
+
+    [ ! -f input/${YEAR}/${day}.txt ] && cargo aoc input --day "${dayi}"
+    [ ! -f input/${YEAR}/${day}.txt.gpg ] && need_hiding=1
 done
+
+if [ "${need_hiding}" -eq 1 ]
+then
+    ./hide_input.sh
+fi
